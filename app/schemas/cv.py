d88cc98c5ca93d8eba36592a0824036c5ba5305e
gidaszewski <gidaszewskifranco@gmail.com>
2025-05-27 20:18:42 +0200

    Refactor Cv schema: rename CvBase to PersonalData and remove unused classes

--- app/schemas/cv.py
+++ app/schemas/cv.py
@@ -1,9 +1,7 @@
-from datetime import datetime
 from pydantic import BaseModel, EmailStr, HttpUrl, Field
-from .user import UserResponse
 
 
-class CvBase(BaseModel):
+class PersonalData(BaseModel):
     first_name: str = Field(..., max_length=50)
     last_name: str = Field(..., max_length=50)
     email_address: EmailStr | None = None
@@ -13,17 +11,3 @@ class CvBase(BaseModel):
     country: str = Field(..., max_length=80)
     city: str = Field(..., max_length=80)
     summary: str
-
-
-class CvCreate(CvBase):
-    user_id: int
-
-
-class CvResponse(CvBase):
-    id: int
-    created_at: datetime
-    updated_at: datetime
-    user: UserResponse
-
-    class Config:
-        orm_mode = True
