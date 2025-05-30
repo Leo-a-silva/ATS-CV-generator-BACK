00c82e1ef5f8b8bcfed9fc5220ed56f7920d5821
gidaszewski <gidaszewskifranco@gmail.com>
2025-05-30 16:13:15 +0200

    Red test for acceptance test

---
+++ src/cvs/tests/infrastructure/api/test_create_cv.py
@@ -0,0 +1,39 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_creates_cv_and_returns_200_ok():
+    cv_input = {
+        "user_id": 1,
+        "first_name": "Alex",
+        "last_name": "Caniggia",
+        "email_address": "alex.caniggia@example.com",
+        "phone_number": "+543434586789",
+        "linkedin_url": "https://linkedin.com/",
+        "portfolio_url": "https://ats.com/",
+        "country": "ARG",
+        "city": "Buenos Aires",
+        "summary": "Star",
+    }
+
+    res = {
+        "cv_id": 1,
+        "data": {
+            "user_id": 1,
+            "first_name": "Alex",
+            "last_name": "Caniggia",
+            "email_address": "alex.caniggia@example.com",
+            "phone_number": "+543434586789",
+            "linkedin_url": "https://linkedin.com/",
+            "portfolio_url": "https://ats.com/",
+            "country": "ARG",
+            "city": "Buenos Aires",
+            "summary": "Star",
+        },
+    }
+
+    response = client.post("/cvs/", data=cv_input)
+    assert response.status_code == 200
+    assert response.json() == res
